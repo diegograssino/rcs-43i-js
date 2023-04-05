a6992a5d6bb9b1e57fe786ccfda6bdbb3568ed1f
diegograssino <diegograssino@gmail.com>
2023-04-05 18:58:17 -0300

    Primera clase

---
+++ temas/tema1.js
@@ -0,0 +1,57 @@
+// Mensajes por consola y alert
+
+// Definición de variables, formas y sintaxis
+
+// Lo definido con "let" puede variar.
+
+// Lo definido con "const" no va a variar (OJO! si es un objeto igual lo defino con cost y ahi SI puede variar)
+
+// Lo definido con "var" puede variar, entonces, cual es la diferencia con let? Que con var los valores existen durante toda la vida de la aplicacion, con let puedo achicar el ambito de accion.
+
+// Siempre debo usar camelCase para los nombres de las variables, las constantes se definen con mayusculas.
+
+// Number
+
+// Boolean
+
+// Array o arreglos o listas
+
+// Object
+
+// 1) Defina una variable sin asignarle ningún valor.
+
+// A la variable del enunciado anterior, asignele el valor "cinco".
+
+// Diferencia entre "cinco", "5" y 5 (los tipos/ valores son diferentes)
+
+// let concatenacion = valor + " " + "es el valor";
+
+// Muestre por consola la variable de los enunciados anteriores.
+
+// 2) Defina dos variables y asigne un valor del tipo number a elección a cada una.
+
+// Sume de uno en uno una variable incrementadola en 5
+
+// Sume las variables del enunciado anterior y asigne el resultado a una tercera variable.
+
+// Muestre por consola el resultado.
+
+// 3) Defina una constante y una variable con valores de tipo number a elección.
+
+// Sume el valor de la constante incrementado en 1 con el de la variable
+
+// Ahora multiplique el valor de la variable por 10, y luego, sume nuevamente el valor de la constante con el de la variable.
+
+// Finalmente, asigne cada uno de los resultados de las operaciones y muestrelos por consola.
+
+// 5) Compare 5 y "5" e indique si son iguales en formato booleano, a su vez, compare "5" con 5 haciendo la misma operación.
+
+// 6) Compare la igualdad 5 y "5" de una manera que el resultado sea false.
+
+// 7) Compare si 5 y 5 son desiguales.
+
+// 8) Declare una variable booleana, invierta su valor y muestrela por consola.
+
+// 9) Compare si 10 es menor que 100, y al mismo tiempo, si 100 es menor que 10. Muestre el resultado en formato booleano por consola.
+
+// 10) Compare si 10 es menor que 100, y a su vez, si 100 es menor que 1, si uno de las dos comparaciones es cierta, el resultado debe ser cierto. Muestre el resultado en formato booleano por consola.
