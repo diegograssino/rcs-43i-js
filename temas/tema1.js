0f5d14f485add6cfb8f4b2ae2748ef74a1f33b8a
Diego Grassino <diegograssino@gmail.com>
2023-04-19 21:42:44 -0300

    Fin de la clase

--- temas/tema1.js
+++ temas/tema1.js
@@ -42,6 +42,8 @@
 // const variableEnString = 'cinco';
 // const variableEnStringTramposa = '5';
 // const variableEnNumber = 5;
+// const variableTramposa = false;
+// console.log(typeof variableTramposa);
 
 // Diferencia entre "cinco", "5" y 5 (los tipos/ valores son diferentes)
 
@@ -53,34 +55,92 @@
 //   variableEnStringTramposa === variableEnNumber
 // );
 
-// let concatenacion = valor + " " + "es el valor";
-
 // Muestre por consola la variable de los enunciados anteriores.
 
 // 2) Defina dos variables y asigne un valor del tipo number a elección a cada una.
 
+// let number1 = 5;
+// console.log('Valor de number1: ' + number1);
+// let number2 = 10;
+// console.log('Valor de number2: ' + number2);
 // Sume de uno en uno una variable incrementadola en 5
+
+// console.log(
+//   'Valor de number1 + 1: ' + (number1 + 1)
+// );
+
+// number1 = number1 + 1;
+// console.log(
+//   'Valor de number1 despues de la suma: ' +
+//     number1
+// );
 
 // Sume las variables del enunciado anterior y asigne el resultado a una tercera variable.
 
+// let sum = number1 + number2;
+
 // Muestre por consola el resultado.
+// console.log(
+//   'El valor de la suma de number1 y number2: ' +
+//     sum
+// );
+
+// sum = sum + 1;
+// sum++;
+// console.log(sum);
+
+// const primeraParte = 'Comisión';
+
+// console.log(primeraParte + ' ' + '43i');
 
 // 3) Defina una constante y una variable con valores de tipo number a elección.
 
+// const number1 = 15;
+// let number2 = 30;
+
 // Sume el valor de la constante incrementado en 1 con el de la variable
 
+// const number3 = number1 + 1;
+
+// const sum = number1 + 1 + number2;
+// console.log('sum: ' + sum);
+
 // Ahora multiplique el valor de la variable por 10, y luego, sume nuevamente el valor de la constante con el de la variable.
+// const mult = number2 * 10;
+// console.log('mult: ' + mult);
+// case sensitive
+// camelCase
 
 // Finalmente, asigne cada uno de los resultados de las operaciones y muestrelos por consola.
 
 // 5) Compare 5 y "5" e indique si son iguales en formato booleano, a su vez, compare "5" con 5 haciendo la misma operación.
+
+// = Es asignación (acción)
+// == Es igualdad no estricta (operacion logica)
+// === Es igualdad estricta (operacion logica)
+// * Es multiplicar (operacion matematica)
+// / Es dividir (operacion matematica)
+// % Es el módulo (operacion matematica)
+// console.log(4 === '4');
 
 // 6) Compare la igualdad 5 y "5" de una manera que el resultado sea false.
 
 // 7) Compare si 5 y 5 son desiguales.
 
 // 8) Declare una variable booleana, invierta su valor y muestrela por consola.
+// const booleana = true;
+// console.log(booleana);
+// console.log(!booleana);
+// console.log(booleana);
+// ! Es la operación lógica NOT, o negación (operación lógica)
 
 // 9) Compare si 10 es menor que 100, y al mismo tiempo, si 100 es menor que 10. Muestre el resultado en formato booleano por consola.
+// console.log(10 < 100);
+// < > =< => son operaciones lógicas de comparación, menor, mayor, menor o igual, mayor o igual.
 
 // 10) Compare si 10 es menor que 100, y a su vez, si 100 es menor que 1, si uno de las dos comparaciones es cierta, el resultado debe ser cierto. Muestre el resultado en formato booleano por consola.
+
+// && Es la operación lógica AND, ambas partes deben ser true para devolver true.
+// || Es la operación lógica OR, con que una de las dos partes sea true, devuelve true.
+
+// console.log(10 < 100 || 100 < 1);
---
+++ temas/tema2.js
@@ -0,0 +1,168 @@
+// 1)
+// Una en uno solo los strings "La vida es" y "bella", muestre el resultado por consola y con un alert.
+// const string1 = 'La vida es';
+// const string2 = 'bella 2';
+// const envioGratis = true;
+// console.log(string1 + ' ' + string2);
+// alert(string1 + ' ' + string2);
+// console.log('Otra línea');
+// props
+
+// Ahora utilice template literals para hacer lo mismo.
+// console.log(
+//   `El envío es ${
+//     envioGratis
+//       ? 'Envio Gratis'
+//       : 'Envio NO Gratis'
+//   }`
+// );
+
+// Lleve una cadena de caracteres en minuscula a mayusculas.
+// const stringMinuscula = 'esto esta en minúsculas';
+// console.log(stringMinuscula);
+// console.log(stringMinuscula.toUpperCase());
+
+// // Lleve una cadena de caracteres en mayúscula a minúsculas.
+// const lobelto = 'ESTO ESTA EN MAYUSCULAS';
+// console.log(lobelto);
+// console.log(lobelto.toLowerCase());
+
+// mail@gmail.com
+// Mail@gmail.com
+// MAIL@GMAIL.com
+
+// Indique el largo de una cadena de caracteres ingresada con prompt y muestre el resultado con un alert.
+
+// Del mismo string, extraiga las últimas dos letras.
+
+// 3)
+// Truncar un número no entero.
+
+// Redondear al más cercano un número no entero
+
+// Devuelva el máximo valor de 5 variables (1, 5, 100, 2, 10)
+
+// let varMaxNumeros = Math.max(1, 5, 1000, 2, 10)
+
+// Con las mismas variables, devuelva el mínimo
+
+// Ahora calcule la raiz cuadrada de 12
+
+// Calcule el resultado de 8 elevado a la 10ma potencia
+
+// Sume el número 10 con uno generado aleatoriamente
+
+// let numeroAleatorioCustom = Math.floor(Math.random() * max)
+
+// A ese número redondearlo a dos decimales
+
+// 4)
+// Dado un número, indicar si ese número es mayor a 10.
+
+// Dado un número, indicar si ese número es menor o igual a 10.
+
+// Dado un número, indicar si ese número es par.
+
+// Dado un número, indicar si ese número es impar.
+
+// Ante un valor ingresado, indicar si tiene contenido.
+
+// 5)
+
+// Dado un número, indicar si ese número es mayor o menor o igual
+// a 10.
+
+// let numero = prompt('Ingrese un número: ');
+
+// Dado un número, indicar si ese número es par o impar.
+
+// Dados dos números, indicar cual es el mayor.
+
+// Ante un valor ingresado, indicar si tiene o no contenido.
+
+// 6)
+
+// Dados dos números, indicar cual es mayor o si son iguales.
+
+// Pedir las tres notas de un alumno y calcular su promedio, si es mayor
+// a 6, mostrar que aprobó, si es menor a 6, mostrar que desaprobó
+// y si es igual a 6, mostrar que está en condiciones de recuperación.
+
+// Prompt siempre me devuelve un string
+
+// 7)
+// Crear un algoritmo que pida al usuario el diámetro de una rueda
+// y su grosor (en metros) y realice las siguientes operaciones:
+
+// a) Si el diámetro es superior a 1.4 debe mostrarse el mensaje
+//“La rueda es para un vehículo grande”. Si es menor o igual a 1.4
+// pero mayor que 0.8 debe mostrarse el mensaje “La rueda es para
+// un vehículo mediano”. Si no se cumplen ninguna de las condiciones
+// anteriores debe mostrarse por pantalla el mensaje “La rueda es
+//para un vehículo pequeño”.
+
+// b) Si el diámetro es superior a 1.4 con un grosor inferior a 0.4,
+// ó si el diámetro es menor o igual a 1.4 pero mayor que 0.8, con un
+// grosor inferior a 0.25, deberá mostrarse el mensaje “El grosor para
+// esta rueda es inferior al recomendado”
+
+// Solicitar un numero de mes y mostrar el nombre del mes
+// correspondiente.
+
+// let mes = 20;
+
+// if (mes === 1) {
+//   console.log('El mes es Enero');
+// } else if (mes === 2) {
+//   console.log('El mes es Febrero');
+// } else if (mes === 3) {
+//   console.log('El mes es Marzo');
+// } else if (mes === 4) {
+//   console.log('El mes es Abril');
+// } else if (mes === 5) {
+//   console.log('El mes es Mayo');
+// } else if (mes === 6) {
+//   console.log('El mes es Junio');
+// } else if (mes === 7) {
+//   console.log('El mes es Julio');
+// } else if (mes === 8) {
+//   console.log('El mes es Agosto');
+// } else if (mes === 9) {
+//   console.log('El mes es Septiembre');
+// } else if (mes === 10) {
+//   console.log('El mes es Octubre');
+// } else if (mes === 11) {
+//   console.log('El mes es Noviembre');
+// } else if (mes === 12) {
+//   console.log('El mes es Diciembre');
+// } else {
+//   console.log('No corresponde a ningún mes');
+// }
+
+// Solicitar un numero de día y mostrar el nombre del día
+
+// 8)
+// Hacer ahora con Switch:
+// Pedir las tres notas de un alumno y calcular su promedio, si es mayor
+// a 6, mostrar que aprobó, si es menor a 6, mostrar que desaprobó
+// y si es igual a 6, mostrar que está en condiciones de recuperación.
+
+// Hacer ahora con Switch:
+// Solicitar un numero de mes y mostrar el nombre del mes
+// correspondiente.
+
+// Hacer ahora con Switch:
+// Solicitar un numero de día y mostrar el nombre del día
+
+// Escribe un programa que responda a un usuario que quiere comprar
+// un helado en una conocida marca de comida rápida cuanto le costará
+// en función del topping que elija. Si no hay concidencias debe
+// informarlo.
+// El helado sin topping cuesta $100.
+// El topping de oreo cuesta $150.
+// El topping de KitKat cuesta $175.
+// El topping de brownie cuesta $175.
+
+// Solicitar al usuario un modelo de auto que desea comprar e
+
+// informarle cuánto cuesta. Si no hay concidencias debe informarlo.
